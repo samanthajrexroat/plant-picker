2cddb9b435a94efca15afc6fda598954934c900d
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-07-07 13:27:27 -0600

    using callback for log

--- src/App.js
+++ src/App.js
@@ -23,8 +23,17 @@ class App extends Component {
             Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
           <button onClick={()=> {
-            this.setState({name: {firstName: "Kat", lastName: "Valken"}});
-            console.log(this.state);
+            this.setState(
+              () => {
+                return {
+                  name: {firstName: "Kat", lastName: "Valken"},
+                };
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
+            
           }}>
             Change Name
           </button>
