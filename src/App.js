9ba2028a177deac5a3435311c5c3745f60828515
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-07-08 13:00:57 -0600

    finished api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
-import { render } from '@testing-library/react';
+
+import "./App.css";
+
 
 class App extends Component {
   constructor() {
@@ -13,29 +13,33 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then((users) => console.log(users))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { plants: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
-
     return (
       <div className="App">
-        {
-          this.state.plants.map((plant) => {
-            return (
-              <div key={plant.id}>
-                <h1>{plant.name}</h1>
-              </div>
-            );
-          })
-        }
+        {this.state.plants.map((plant) => {
+          return (
+            <div key={plant.id}>
+              <h1>{plant.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
-
-
 
 export default App;
