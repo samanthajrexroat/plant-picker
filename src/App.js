fd6426622adb6ecb8ba835322ebca8a1ba8f5147
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-07-11 13:23:03 -0600

    Moved event handler out of render

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super();
     this.state = {
       plants: [],
+      searchField: ''
     };
     console.log('constructor')
   }
@@ -29,24 +30,28 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    // [{ name: 'Leanne'}, { name: 'Sam'}]
+  
+    // Update state with the new array of plants from the search.
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
   render() {
     console.log('render');
+
+    const filteredPlants = this.state.plants.filter((plant) => {
+      // if the name of the plant includes the search request, create a new array of those plants.
+      return plant.name.toLocaleLowerCase().includes(this.state.searchField)
+    });
+
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder="search plants" onChange={(event) => {
-          console.log(event.target.value);
-          const searchString = event.target.value.toLocaleLowerCase();
-          // [{ name: 'Leanne'}, { name: 'Sam'}]
-          const filteredPlants = this.state.plants.filter((plant) => {
-            // if the name of the plant includes the search request, create a new array of those plants.
-            return plant.name.toLocaleLowerCase().includes(searchString)
-          });
-          // Update state with the new array of plants from the search.
-          this.setState(() => {
-            return { plants: filteredPlants };
-          })
-        }}/>
-        {this.state.plants.map((plant) => {
+        <input className='search-box' type='search' placeholder="search plants" onChange={this.onSearchChange}/>
+        {filteredPlants.map((plant) => {
           return (
             <div key={plant.id}>
               <h1>{plant.name}</h1>
